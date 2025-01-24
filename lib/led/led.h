3b0f037497a87aacda7b111a76586db00b2a8cb7
shhds <1873145421@qq.com>
2025-01-24 16:09:28 +0800

    first commit

---
+++ lib/led/led.h
@@ -0,0 +1,14 @@
+#ifndef _LED_H_
+#define _LED_H_
+
+void ledPowerOn(void);
+void ledLowPower(void);
+void ledCharging(void);
+void ledCharged(void);
+void powerOff(void);
+
+void led_OFF_ACT(void);
+void led_ON_ACT(void);
+
+
+#endif
